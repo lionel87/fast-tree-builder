cccb58a877124d76ac8a6fed8973b8df9dde2ad4
root <root@localhost>
2025-02-17 21:40:11 +0100

    add more tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -92,6 +92,41 @@ describe('buildTree', () => {
 		assert.strictEqual(roots[0].children[1].parent, undefined);
 	});
 
+	it('should merge the node data into the nodes (parent mode)', () => {
+		const items = [
+			{ id: 1, parent: null, name: 'Root 1' },
+			{ id: 2, parent: null, name: 'Root 2' },
+			{ id: 3, parent: 1, name: 'Child 1.1' },
+			{ id: 4, parent: 1, name: 'Child 1.2' },
+			{ id: 5, parent: 2, name: 'Child 2.1' },
+		];
+
+		const { roots } = buildTree(items, {
+			nodeDataKey: false,
+		});
+
+		assert.strictEqual(roots[0].children[0].name, 'Child 1.1');
+		assert.strictEqual(roots[0].children[1].name, 'Child 1.2');
+	});
+
+	it('should merge the node data into the nodes (children mode)', () => {
+		const items = [
+			{ id: 1, children: [3, 4], name: 'Root 1' },
+			{ id: 2, children: [5], name: 'Root 2' },
+			{ id: 3, name: 'Child 1.1' },
+			{ id: 4, name: 'Child 1.2' },
+			{ id: 5, name: 'Child 2.1' },
+		];
+
+		const { roots } = buildTree(items, {
+			mode: 'children',
+			nodeDataKey: false,
+		});
+
+		assert.strictEqual(roots[0].children[0].name, 'Child 1.1');
+		assert.strictEqual(roots[0].children[1].name, 'Child 1.2');
+	});
+
 	it('should map node data when mapper fn configured', () => {
 		const items = [
 			{ id: 1, parent: null, name: 'Root 1' },
@@ -151,6 +186,28 @@ describe('buildTree', () => {
 		);
 	});
 
+	it('should validate root keys without thrown error when no error', () => {
+		const items = [
+			{ id: 1, parent: null, name: 'Item 1' },
+			{ id: 2, parent: 1, name: 'Item 2' },
+		];
+
+		const { roots } = buildTree(items, { validRootKeys: [1] });
+
+		assert.deepStrictEqual(roots[0].children[0].data.name, 'Item 2');
+	});
+
+	it('should validate root parent keys without thrown error when no error', () => {
+		const items = [
+			{ id: 1, parent: null, name: 'Item 1' },
+			{ id: 2, parent: 1, name: 'Item 2' },
+		];
+
+		const { roots } = buildTree(items, { validRootParentKeys: [null] });
+
+		assert.deepStrictEqual(roots[0].children[0].data.name, 'Item 2');
+	});
+
 	it('should handle invalid parent keys and throw an error', () => {
 		const items = [
 			{ id: 1, parent: null, name: 'Item 1' },
@@ -174,6 +231,32 @@ describe('buildTree', () => {
 			() => buildTree(items, { validRootKeys: ['c'] }),
 			Error,
 			'A root node has an invalid key "a"'
+		);
+	});
+
+	it('should throw when validate root parent keys set with mode: children', () => {
+		const items = [
+			{ id: 1, parent: null, name: 'Item 1' },
+			{ id: 2, parent: 1, name: 'Item 2' },
+		];
+
+		assert.throws(
+			() => buildTree(items, { validRootParentKeys: [null], mode: 'children' }),
+			Error,
+			'Option "validRootParentKeys" cannot be used when mode is set to "children".'
+		);
+	});
+
+	it('should throw when duplicate ids found', () => {
+		const items = [
+			{ id: 1, parent: null, name: 'Item 1' },
+			{ id: 1, parent: null, name: 'Item 2' },
+		];
+
+		assert.throws(
+			() => buildTree(items, { mode: 'children' }),
+			Error,
+			'Duplicate identifier detected for "1"'
 		);
 	});
 });
