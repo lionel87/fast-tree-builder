605205521b60b0a55be40965946cc01393696e5e
root <root@localhost>
2025-03-11 20:37:13 +0100

    minor test improvement

--- test/index.spec.js
+++ test/index.spec.js
@@ -150,6 +150,7 @@ describe('buildTree', () => {
 		const items = [
 			{ id: 1, parent: 2, name: 'Item 1' },
 			{ id: 2, parent: 1, name: 'Item 2' },
+			{ id: 3, name: 'Item 3' }, // a distinct tree root to be sure we handle forests.
 		];
 
 		assert.throws(
